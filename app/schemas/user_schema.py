692b26f9d0767603811a222d227963050b15880a
Vijay-3253 <vijay@qpactsolutions.com>
2026-05-23 11:30:12 +0530

    feat: implement POST user API with validation and dependency setup

--- app/schemas/user_schema.py
+++ app/schemas/user_schema.py
@@ -1,11 +1,17 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 
 
 # ---------------------------------
-# User Request Schema
+# USER CREATE VALIDATION SCHEMA
 # ---------------------------------
 
 class UserCreate(BaseModel):
 
-    name: str
-    email: str
+    name: str = Field(
+        ...,
+        min_length=3,
+        max_length=50,
+        description="User full name must be 3-50 characters"
+    )
+
+    email: EmailStr   # automatically validates email format
